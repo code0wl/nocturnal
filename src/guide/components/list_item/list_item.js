ff59d65f9127bb905a820ef2ada085440fbf69ac
code0wl <o.lodriguez@gmail.com>
2017-10-20 08:00:02 +0200

    bug in render

--- src/guide/components/list_item/list_item.js
+++ src/guide/components/list_item/list_item.js
@@ -1,21 +1,10 @@
 import React from "react";
-import * as components from "../../materials.json";
-import Link from "react-router-dom/es/Link";
 
-export default () => {
-    return components.materials.map(component => {
-        const objs = Object.keys(component);
-        return objs.map((material) => {
-            return component[material].map((item) => {
-                return item.items.map((prop, index) => {
-                    return (
-                        <Link key={index} to={prop.material + "-" + item}>
-                            <li key={prop+index}>{prop}</li>
-                        </Link>
-                    );
-                })
-            });
-        })
-
+export default ({items}) => {
+    console.log(items[0])
+    return items.items.map(material => {
+        return (
+            <li> {material} </li>
+        );
     });
 }
--- src/guide/components/side_bar/side_bar.js
+++ src/guide/components/side_bar/side_bar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ListItem from "../list_item/list_item";
+import * as components from "../../materials.json";
 import "./side_bar.css";
 
 export default () => {
-    return (
-        <ul>
-            <ListItem/>
-        </ul>
-    );
+    return components.materials.map((component) => {
+        const objs = Object.keys(component);
+        return objs.map((material) => {
+            return component[material].map((item) => {
+                return (
+                    <div>
+                        <h4>{material}</h4>
+                        <ul>
+                            <ListItem items={item}/>
+                        </ul>
+                    </div>
+                );
+            });
+        });
+    });
 };
