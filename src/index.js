2e7e22b875a787da4443e6ee7089edfa57b55d8b
code0wl <o.lodriguez@gmail.com>
2017-11-02 08:41:33 +0100

    chip

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import {render} from "react-dom";
 import "./guide/index.css";
 import App from "./guide/app";
-import config from './guide/store/config';
-import {Provider} from 'react-redux';
+import config from "./guide/store/config";
+import {Provider} from "react-redux";
 import registerServiceWorker from "./guide/registerServiceWorker";
 
 const store = config();
