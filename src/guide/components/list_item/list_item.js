c0d341d649a0ff3235bc22f04172e6f0545aa60b
code0wl <o.lodriguez@gmail.com>
2017-11-02 20:40:38 +0100

    added types

--- src/guide/components/list_item/list_item.js
+++ src/guide/components/list_item/list_item.js
@@ -5,7 +5,10 @@ export default ({materials, select}) => {
     return materials.map((material, index) => {
         return (
             <li key={index}>
-                <Link className={window.location.pathname === "/" + material.exportedComponent ? "is-selected" : ""} onClick={select} to={material.exportedComponent}> {material.exportedComponent} </Link>
+                <Link className={window.location.pathname === "/" + material.exportedComponent ? "is-selected" : ""} onClick={select} to={material.exportedComponent}>
+                    {material.exportedComponent}
+                    <span data-badge-caption="" className="new badge blue"> {material.type} </span>
+                </Link>
             </li>
         );
     });
