3e13dac96ba8e151324428f0b4f21a7197903a88
code0wl <o.lodriguez@gmail.com>
2017-11-12 18:00:11 +0100

    canvas

--- src/guide/components/canvas/canvas.js
+++ src/guide/components/canvas/canvas.js
@@ -7,8 +7,8 @@ export default class Canvas extends Component {
     render() {
         return (
             <main style={{order: '3', transition: 'transform .4s', padding: '2rem', flexGrow: '1', height: '100vh', overflow: 'auto', transform: 'translateX(10px)'}}>
-                <article className="canvas-component">
-                    <h3 className="component-title"> {window.location.pathname.replace("/", "")} </h3>
+                <article>
+                    <h3 className={{color: '#606e7f', textTransform: 'capitalize', margin: '0', paddingBottom: '5rem'}}> {window.location.pathname.replace("/", "")} </h3>
                     <Route component={SiteRoutes}/>
                 </article>
             </main>
