0f3d9aa6db55cdf01c49a183df4d3ed78d350ed4
code0wl <o.lodriguez@gmail.com>
2017-11-12 16:10:19 +0100

    removed depedencies

--- src/guide/components/filter/filter.js
+++ src/guide/components/filter/filter.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./filter.css";
-import Input from "muicss/lib/react/input";
 
 export default ({change}) => {
     return (
         <div className="input-field col s6 filter">
-            <Input hint="Filter on type or name" onChange={change} className="validate"/>
+            <input type="search" hint="Filter on type or name" onChange={change} className="validate"/>
             <i className="mi mi-filter-list"></i>
         </div>
     );
