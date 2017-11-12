25c133b8f205250d67b2f410275145f39c87c1df
code0wl <o.lodriguez@gmail.com>
2017-11-12 17:55:36 +0100

    canvas is now inline

--- src/guide/components/canvas/canvas.js
+++ src/guide/components/canvas/canvas.js
@@ -6,7 +6,7 @@ import SiteRoutes from "../routes/routes";
 export default class Canvas extends Component {
     render() {
         return (
-            <main className="canvas">
+            <main style={{order: '3', transition: 'transform .4s', padding: '2rem', flexGrow: '1', height: '100vh', overflow: 'auto', transform: 'translateX(10px)'}}>
                 <article className="canvas-component">
                     <h3 className="component-title"> {window.location.pathname.replace("/", "")} </h3>
                     <Route component={SiteRoutes}/>
