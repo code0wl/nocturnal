ea0ae1a7a05a62dffc4bab65a963b8c69334c071
code0wl <o.lodriguez@gmail.com>
2018-07-24 12:49:01 +0200

    design change

--- src/guide/components/filter/filter.js
+++ src/guide/components/filter/filter.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-export default ({change}) => {
-    return (
-        <div className="input-field col s6 filter">
-            <input type="search" hint="Filter on type or name" onChange={change} className="validate"/>
-            <i className="mi mi-filter-list"></i>
-        </div>
-    );
-}
+export default ({ change }) => {
+  return (
+    <div className="input-field col s6 filter">
+      <input
+        style={{
+          width: `100%`,
+          border: 0,
+          margin: `10px 0`,
+          padding: `10px`,
+          transition: `all 0.3s cubic-bezier(.64,.09,.08,1)`,
+          background: `linear-gradient(to bottom, rgba(255,255,255,0) 96%, $color 4%)`,
+          backgroundPosition: `-200px 0`,
+          backgroundSize: `200px 100%`
+        }}
+        type="search"
+        placeholder="Filter on type or name"
+        onChange={change}
+        className="validate"
+      />
+      <i className="mi mi-filter-list" />
+    </div>
+  );
+};
--- src/materials/atoms/input/input.js
+++ src/materials/atoms/input/input.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 export class Input extends React.Component {
-    render() {
-        return (
-            <div className="input-field col s6">
-                <input id="first_name" type="text" className="validate"/>
-                <label htmlFor="first_name">First </label>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input
+          id="first_name"
+          type="text"
+          className="validate"
+        />
+        <label htmlFor="first_name">First </label>
+      </div>
+    );
+  }
 }
