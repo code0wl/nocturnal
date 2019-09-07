2a825f6103e019c0c9c58a579c3e0d88d01bc525
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-09-07 14:20:57 +0200

    removed empty components

--- src/examples/atomic_example_project/materials/organisms/form/form.tsx
+++ src/examples/atomic_example_project/materials/organisms/form/form.tsx
@@ -1,39 +1,45 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export class Form extends Component {
-    render() {
-        return (
-            <div className="row">
-                <form className="col s12">
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input id="first_name" type="text" className="validate"/>
-                            <label htmlFor="first_name">First </label>
-                        </div>
-                        <div className="input-field col s6">
-                            <input id="last_name" type="text" className="validate"/>
-                            <label htmlFor="last_name">Last Name</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input disabled value="I am not editable" id="disabled" type="text" className="validate"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="password" type="password" className="validate"/>
-                            <label htmlFor="password">Password</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <input id="email" type="email" className="validate"/>
-                            <label htmlFor="email">Email</label>
-                        </div>
-                    </div>
-                </form>
+  render() {
+    return (
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="first_name" type="text" className="validate" />
+              <label htmlFor="first_name">First </label>
             </div>
-        );
-    }
+            <div className="input-field col s6">
+              <input id="last_name" type="text" className="validate" />
+              <label htmlFor="last_name">Last Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                disabled
+                value="I am not editable"
+                id="disabled"
+                type="text"
+                className="validate"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="password" type="password" className="validate" />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="email" type="email" className="validate" />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
